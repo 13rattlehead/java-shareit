094cff9368beca57a4b02952b78de6a5a96804b1
13rattlehead <rattlehead13@yandex.ru>
2026-08-10 12:46:28 +0300

    исправить codestyle, убрать unused import

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -2,10 +2,8 @@ package ru.practicum.shareit.booking.service;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
-import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingState;
 
-import java.util.Collection;
 import java.util.List;
 
 
